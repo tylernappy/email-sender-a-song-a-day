b8234d0e8ce8d5ca74007bac31f28d806f89222c
Tyler Nappy <tylernappy@gmail.com>
2014-11-26 22:43:46 -0500

    fixed link issues in email

--- app/controllers/send_emails_controller.rb
+++ app/controllers/send_emails_controller.rb
@@ -15,9 +15,9 @@ class SendEmailsController < ApplicationController
                :from => "Shannon from A Song A Day <#{ENV['myRegisteredMailjetEmailAtDomainDotCom']}>",
                :subject => "Today's song is...",
                :html => "<p>Hi,</p>\n\n
-               <p>Here's your song of the day, curated by <a href=#{params[:send_email][:twitter_page]} target='_blank'>#{params[:send_email][:curator]}</a>:</p>\n\n
-               <h2><a href=#{params[:send_email][:link]} target='_blank'>#{params[:send_email][:song]} by #{params[:send_email][:band]}</a></h2>\n\n
-               <h3><a href=#{params[:click_to_tweet]} target='_blank'>Tweet</a></h3>\n\n
+               <p>Here's your song of the day, curated by <a href='#{params[:send_email][:twitter_page]}' target='_blank'>#{params[:send_email][:curator]}</a>:</p>\n\n
+               <h2><a href='#{params[:send_email][:link]}' target='_blank'>#{params[:send_email][:song]} by #{params[:send_email][:band]}</a></h2>\n\n
+               <h3><a href='#{params[:send_email][:click_to_tweet]}' target='_blank'>Tweet</a></h3>\n\n
                <p>#{params[:send_email][:message]}</p>\n\n
                <p>Let me know what you think of the song. Happy listening!</p>"
                # :subject => EmailTemplate.first.subject,
